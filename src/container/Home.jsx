e3ae83cc6e109cb654c685ab19e3f07b197e298a
Alexandr <sparkygino@gmail.com>
2023-06-12 20:47:05 +0300

    test fetch

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,10 +1,23 @@
-
 const Home = () => {
+  function checkServer() {
+    let order = fetch('https://api.skilla.ru/testapi', {
+      method: 'POST',
+      headers: {
+        'Authentication': 'Bearer testtoken',
+        'Acess-Control-Allow-Headers': 'API-Key,Content-Type,If-Modified-Since,Cache-Control',
+      },
+    });
+
+    console.log(order);
+  }
+
   return (
     <>
-        <div className="container"></div>
+      <div className="container">
+        <button onClick={checkServer}>Test</button>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
